33716674ab7ec880be5fe55f9f5bd4cd955ca73d
ashaw1270 <127970321+ashaw1270@users.noreply.github.com>
2023-03-15 14:06:34 -0500

    Update SAMO.py

--- SAMO.py
+++ SAMO.py
@@ -2,6 +2,7 @@ import csv
 from tensorflow import keras
 import numpy as np
 
+# import csv files and convert to numpy arrays
 with open(file='Training_X.csv', newline='', encoding='utf-8-sig') as f:
     reader = csv.reader(f)
     x_train = np.float_(list(reader))
@@ -18,20 +19,21 @@ with open(file='Tests_Y.csv', newline='', encoding='utf-8-sig') as f:
     reader = csv.reader(f)
     y_test = np.float_(list(reader))
 
+# create the model
 model = keras.Sequential([
     keras.layers.Dense(units=2, activation='sigmoid', input_shape=(2,)),
     keras.layers.Dense(units=128, activation='sigmoid'),
     keras.layers.Dense(units=2, activation='softmax')
 ])
 
-# Compile the model
+# compile the model
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['binary_accuracy'])
 
-# Train the model
+# train the model
 model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
 
-# Evaluate the model on the test dataset
+# evaluate the model on the test data
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print('Test accuracy:', test_acc)
